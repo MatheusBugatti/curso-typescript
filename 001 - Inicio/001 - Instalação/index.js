0365796f84a4257608137b8a179281c770bd583b
matheus <matheus.info@mazatarraf.com.br>
2024-02-28 13:28:03 -0300

    modulo de Compilador

--- 001 - Inicio/001 - Instalação/index.js
+++
@@ -1,1 +0,0 @@
-console.log("npm install -g typescript");
---
+++ 001 - Inicio/003 - Classes/008 - reandonly/index.ts
@@ -0,0 +1,8 @@
+class Pessoa{
+    public readonly nome: string = "Matheus Bugatti"
+}
+
+let pessoa = new Pessoa();
+
+console.log(pessoa);
+//pessoa.nome = ""
